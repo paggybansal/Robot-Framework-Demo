7568a942cd9df279e8eb7af0da722b3043acaee6
parag_bansal <parag_bansal@epam.com>
2025-09-11 12:47:00 +0530

    robot framework

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
 import os
 ORACLE_CLIENT_PATH = '/path/to/oracle/client'
 os.environ['ORACLE_CLIENT_PATH'] = ORACLE_CLIENT_PATH
+
+ORACLE_DB_PASSWORD = 'your_password_here'
+POSTGRES_DB_PASSWORD = 'your_password_here'
